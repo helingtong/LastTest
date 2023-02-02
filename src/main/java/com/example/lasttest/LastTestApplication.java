3f89c7ddd834601cafca4ac5206fd2ab4a0c464c
xiaohe <3128324742@qq.com>
2023-02-02 13:40:40 +0800

    第二次提交

---
+++ src/main/java/com/example/lasttest/LastTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.lasttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lasttest/LastTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lasttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
